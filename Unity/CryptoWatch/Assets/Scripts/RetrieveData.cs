e6f02349cca95ffd7b641069d0bdb88ad773780f
NerbzHub <nathan.nette@gmail.com>
2018-12-21 23:06:06 +1100

    The latest prices currently get displayed in the console.

---
+++ Unity/CryptoWatch/Assets/Scripts/RetrieveData.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class RetrieveData : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(GetText());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator GetText()
+    {
+        UnityWebRequest www = UnityWebRequest.Get("https://www.coinspot.com.au/pubapi/latest");
+        Debug.Log(www.ToString());
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            // Show results as text
+            Debug.Log(www.downloadHandler.text);
+
+            // Or retrieve results as binary data
+            byte[] results = www.downloadHandler.data;
+        }
+    }
+}
